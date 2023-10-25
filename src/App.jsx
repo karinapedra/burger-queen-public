2e7dc0b4285aa44b98e739b70a733b82e4614855
karinapedra <karina_kap@hotmail.com>
2023-10-24 21:30:22 -0300

    T002 Feat: Initial React setup, added login page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
+import RoutesApp from './routes';
+import { AuthProvider } from './contexts/auth';
 
-const App = ()  => {
-  return <div>Burger Queen</div>;
+const App = () => {
+  return (
+    <AuthProvider>
+      <RoutesApp />
+      <GlobalStyle />
+    </AuthProvider>
+  );
 };
 
 export default App;
---
+++ src/pages/Signin/index.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import * as C from './styles';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+
+const Signin = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = () => {
+    if (!email | !password) {
+      setError('Fill in all the fields');
+      return;
+    }
+
+    const res = signin(email, password);
+
+      if(res) {
+        setError(res);
+        return;
+      }
+
+    navigate('/home');
+  };
+
+  return (
+    <C.Container>
+      <C.Label>Login System</C.Label>
+      <C.Content>
+        <Input
+          type='email'
+          placeholder='email@example.com'
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError('')]}
+        />
+        <Input
+          type='password'
+          placeholder='Insert your password'
+          value={password}
+          onChange={(e) => [setPassword(e.target.value), setError('')]}
+        />
+        <C.labelError>{error}</C.labelError>
+        <Button Text='Sign in' onClick={handleLogin} />
+        </C.Content>
+    </C.Container>
+  );
+};
+
+export default Signin;
